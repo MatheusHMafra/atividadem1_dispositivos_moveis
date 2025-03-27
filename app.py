d665474bdfbdab4cb4fa07ddee0447b6cbf95762
Matheus <maframatheus982@gmail.com>
2025-03-26 21:11:05 -0300

    Arrumado

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory
 from waitress import serve
-import os
 
 app = Flask(__name__, static_folder='./')
 
 @app.route('/')
 def index():
-    return send_from_directory('./', 'index.html')
+    return send_from_directory('.', 'index.html')
 
 @app.route('/<path:path>')
 def static_files(path):
-    return send_from_directory('./', path)
+    return send_from_directory('.', path)
 
 if __name__ == '__main__':
     print("Servidor iniciado em http://localhost:80")
